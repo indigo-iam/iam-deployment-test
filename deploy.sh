8bb1b91b123b41e97845beedd26e3984eea9a1e6
Marco Caberletti <marco.caberletti@gmail.com>
2016-10-14 13:49:41 +0200

    Add interaction with docker registry

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ TESTSUITE_REPO="${TESTSUITE_REPO:-https://github.com/indigo-iam/iam-robot-testsu
 REPO_BRANCH="${REPO_BRANCH:-master}"
 BROWSER="${BROWSER:-firefox}"
 TIMEOUT="${TIMEOUT:-10}"
+DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-"cloud-vm128.cloud.cnaf.infn.it"}
+
+export IMAGE_TAG=$REPO_BRANCH
 
 netname="iam_default"
 ts_name=iam-ts
@@ -18,11 +21,11 @@ function cleanup(){
 		echo "Caught error! Cleanup..."
 		
 		cd $workdir
-		docker rm $builder_name
 		docker-compose -f iam/docker-compose.yml stop
 		docker-compose -f iam/docker-compose.yml rm -f
 		sh iam-robot-testsuite/docker/selenium-grid/selenium_grid.sh stop
 		docker rm $ts_name
+		docker rm $builder_name
 	fi
 	exit $retcod
 }
@@ -43,23 +46,34 @@ fi
 
 
 ## Compile and bring on IAM
-cd maven/
-sh build-image.sh
-docker run --name=$builder_name \
-	-e REPO=https://github.com/indigo-iam/iam.git \
-	-e REPO_BRANCH=$REPO_BRANCH \
-	italiangrid/iam-builder
+set +e
+docker pull $DOCKER_REGISTRY_HOST/italiangrid/iam-login-service:$IMAGE_TAG
 
-filesdir=$workdir/iam/iam-be/files/
-mkdir -p $filesdir
-
-docker cp $builder_name:/iam/iam-login-service/target/iam-login-service.war $filesdir
-docker cp $builder_name:/iam/docker/saml-idp/idp/shibboleth-idp/metadata/idp-metadata.xml $filesdir
+if [ $? -ne 0 ]; then
+	set -e
+	cd maven/
+	sh build-image.sh
+	docker run --name=$builder_name \
+		-e REPO=https://github.com/indigo-iam/iam.git \
+		-e REPO_BRANCH=$REPO_BRANCH \
+		italiangrid/iam-builder
+	
+	filesdir=$workdir/iam/iam-be/files/
+	mkdir -p $filesdir
+	
+	docker cp $builder_name:/iam/iam-login-service/target/iam-login-service.war $filesdir
+	docker cp $builder_name:/iam/docker/saml-idp/idp/shibboleth-idp/metadata/idp-metadata.xml $filesdir
+	
+	docker rm $builder_name
+	
+	cd $workdir/iam
+	docker-compose build
+fi
+set -e
 
 cd $workdir/iam
-
 docker-compose rm -f
-docker-compose up --build -d
+docker-compose up -d
 
 cd $workdir
 
@@ -80,12 +94,16 @@ cd $workdir
 
 
 ## Run testsuite
-cd iam-robot-testsuite/docker
-./build-image.sh
+set +e
+docker pull $DOCKER_REGISTRY_HOST/italiangrid/iam-robot-testsuite:latest
+if [ $? -ne 0 ]; then
+	cd iam-robot-testsuite/docker
+	./build-image.sh
+	docker tag italiangrid/iam-robot-testsuite $DOCKER_REGISTRY_HOST/italiangrid/iam-robot-testsuite:latest
+fi
 
 cd $workdir
 
-set +e
 docker run --net $DOCKER_NET_NAME \
 	--name=$ts_name \
 	--add-host $IAM_HOSTNAME:$iam_ip \
@@ -95,7 +113,7 @@ docker run --net $DOCKER_NET_NAME \
 	-e REMOTE_URL=http://selenium-hub:4444/wd/hub \
 	-e BROWSER=$BROWSER \
 	-e TIMEOUT=$TIMEOUT \
-	italiangrid/iam-robot-testsuite
+	$DOCKER_REGISTRY_HOST/italiangrid/iam-robot-testsuite:latest
 set -e
 
 
@@ -110,7 +128,6 @@ docker cp $ts_name:/home/tester/iam-robot-testsuite/reports $reportdir
 
 
 ## Stop services and cleanup
-docker rm $builder_name
 docker-compose -f iam/docker-compose.yml stop
 docker-compose -f iam/docker-compose.yml rm -f
 sh iam-robot-testsuite/docker/selenium-grid/selenium_grid.sh stop
